ef933e2f99bd4a3f04ca2afd26a8805d066964ec
Anuj8553 <99608727+Anuj8553@users.noreply.github.com>
2025-08-16 00:14:20 +0530

    Updated UI with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Blogs from './components/Blogs/Blogs'
 import Socials from './components/Socials/Socials'
 import Resume from './components/Resume/Resume'
 import Footer from './components/Footer/Footer'
+import SplashCursor from './react_bits/SplashCursor/SplashCursor.jsx'
+import Aurora from './react_bits/Aurora/Aurora.jsx';
 
 const App = () => {
   let [scrollCaptureI, setScrollCaptureI] = useState(0)
@@ -24,7 +26,7 @@ const App = () => {
     let mouseX = e.clientX - rect.left;
     let mouseY = e.clientY - rect.top;
     overlay.current.style.opacity = `1`;
-    overlay.current.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(255,255,255,0.03) 0%, transparent calc(5% + 0px)) no-repeat border-box border-box`;
+    overlay.current.style.background = `radial-gradient(circle at ${mouseX}px ${mouseY}px, rgba(245, 243, 243, 0.03) 0%, transparent calc(5% + 0px)) no-repeat border-box border-box`;
   }
 
   const openMenu = () => {
@@ -36,7 +38,15 @@ const App = () => {
 
   // onMouseMove={hoverOverlay}
   return (
-    <div ref={overlay} onMouseMove={hoverOverlay} className='app' >
+    <>
+    <SplashCursor />
+    <Aurora className="aurora-container"
+  colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+  blend={0.5}
+  amplitude={1.9}
+  speed={1}
+  />
+    <div ref={overlay} className='app' >
       <Navbar overlay={overlay} hamIcon={hamIcon} line1={line1} line2={line2} NavbarElement={NavbarElement} hamMenu={hamMenu} openMenu={openMenu} />
       <Hero />
       <Experience />
@@ -45,6 +55,7 @@ const App = () => {
       <Blogs/>     
       <Footer />
     </div>
+    </>
 
   )
 }
